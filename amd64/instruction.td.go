c23be52110c13aae833d26cd7ef297e153a9fdf3
theparthka <parthka.2005@gmail.com>
2023-03-19 11:34:40 +0530

    snap

---
+++ amd64/instruction.td.go
@@ -0,0 +1,8 @@
+// Copyright (c) 2023 Parth Degama
+// This code is licensed under MIT license
+
+// instruction table
+
+package amd64
+
+
---
+++ amd64/instructiongen.go
@@ -0,0 +1,9 @@
+// Copyright (c) 2023 Parth Degama
+// This code is licensed under MIT license
+
+// instruction generation
+
+package amd64
+
+
+
