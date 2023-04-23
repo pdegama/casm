468176accb1172f1b0609bb3234a5c00394a2616
MeParthDegama <pthreeh@outlook.com>
2023-04-23 23:18:56 +0530

    snap

---
+++ x86_64/archopcodefind.go
@@ -0,0 +1,11 @@
+// Copyright (c) 2023 Parth Degama
+// This code is licensed under MIT license
+
+// architecture opcode find
+
+package x86_64
+
+// find arch opcode
+func archOpcodeFind() {
+	
+}
