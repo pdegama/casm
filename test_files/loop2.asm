eb4622314ae0a93ba1234f4a5d75e9e247c4256f
Parthka <contact@parthka.com>
2023-06-23 08:30:58 +0530

    add e.g.

---
+++ test_files/loop2.asm
@@ -0,0 +1,20 @@
+%bits 64
+
+mov rax, 1        ; write(
+mov rdi, 1        ;   STDOUT_FILENO,
+mov rsi, $msg      ;   "Hello, world!\n",
+mov rdx, $msglen   ;   sizeof("Hello, world!\n")
+syscall           ; );
+
+$m:
+jmp $m
+
+mov rax, 60       ; exit(
+mov rdi, 0        ;   EXIT_SUCCESS
+syscall           ; );
+
+$msg: 
+  str "Hello, world!"
+  db 10
+$msglen: 
+  db 14
